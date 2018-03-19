f8fb592ab44bda3a846c50661898bb6b69b0ede9
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-19 00:22:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,8 @@ end
 def swap_elements(array)
    array[0],array[1],array[2] = array[0], array[2],array[1]
 end
+
+def swap_elements_from_to(array,index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
